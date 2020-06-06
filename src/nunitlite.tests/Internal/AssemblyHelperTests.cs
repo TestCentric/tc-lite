13b6a63bcdbfbd2c6b38ab4c7f24d446e2e59632
Charlie Poole <charliepoole@gmail.com>
2020-06-05 18:34:04 -0700

    Add AssemblyHelper

---
+++ src/nunitlite.tests/Internal/AssemblyHelperTests.cs
@@ -0,0 +1,63 @@
+﻿// ***********************************************************************
+// Copyright (c) Charlie Poole and TestCentric contributors.
+// Licensed under the MIT License. See LICENSE.txt in root directory.
+// ***********************************************************************
+
+using System.IO;
+using System.Reflection;
+using NUnit.Framework;
+
+namespace TCLite.Framework.Internal
+{
+    public class AssemblyHelperTests
+    {
+        [Test]
+        public void GetPathForAssembly()
+        {
+            string path = AssemblyHelper.GetAssemblyPath(this.GetType().Assembly);
+            Assert.That(Path.GetFileName(path), Is.EqualTo("nunitlite.tests.dll"));
+            Assert.That(File.Exists(path));
+        }
+
+        //[Test]
+        //public void GetPathForType()
+        //{
+        //    string path = AssemblyHelper.GetAssemblyPath(this.GetType());
+        //    Assert.That(Path.GetFileName(path), Is.EqualTo("nunitlite.tests.exe").IgnoreCase);
+        //    Assert.That(File.Exists(path));
+        //}
+		
+        // The following tests are only useful to the extent that the test cases
+        // match what will actually be provided to the method in production.
+        // As currently used, NUnit's codebase can only use the file: schema,
+        // since we don't load assemblies from anything but files. The uri's
+        // provided can be absolute file paths or UNC paths.
+
+        // Local paths - Windows Drive
+        // [TestCase(@"file:///C:/path/to/assembly.dll", @"C:\path\to\assembly.dll")]
+        // [TestCase(@"file:///C:/my path/to my/assembly.dll", @"C:/my path/to my/assembly.dll")]
+        // [TestCase(@"file:///C:/dev/C#/assembly.dll", @"C:\dev\C#\assembly.dll")]
+        // [TestCase(@"file:///C:/dev/funnychars?:=/assembly.dll", @"C:\dev\funnychars?:=\assembly.dll")]
+        // Local paths - Linux or Windows absolute without a drive
+        [TestCase(@"file:///path/to/assembly.dll", @"/path/to/assembly.dll")]
+        [TestCase(@"file:///my path/to my/assembly.dll", @"/my path/to my/assembly.dll")]
+        [TestCase(@"file:///dev/C#/assembly.dll", @"/dev/C#/assembly.dll")]
+        [TestCase(@"file:///dev/funnychars?:=/assembly.dll", @"/dev/funnychars?:=/assembly.dll")]
+        // Windows drive specified as if it were a server - odd case, sometimes seen
+        // [TestCase(@"file://C:/path/to/assembly.dll", @"C:\path\to\assembly.dll")]
+        // [TestCase(@"file://C:/my path/to my/assembly.dll", @"C:\my path\to my\assembly.dll")]
+        // [TestCase(@"file://C:/dev/C#/assembly.dll", @"C:\dev\C#\assembly.dll")]
+        // [TestCase(@"file://C:/dev/funnychars?:=/assembly.dll", @"C:\dev\funnychars?:=\assembly.dll")]
+        // UNC format with server and path
+        [TestCase(@"file://server/path/to/assembly.dll", @"//server/path/to/assembly.dll")]
+        [TestCase(@"file://server/my path/to my/assembly.dll", @"//server/my path/to my/assembly.dll")]
+        [TestCase(@"file://server/dev/C#/assembly.dll", @"//server/dev/C#/assembly.dll")]
+        [TestCase(@"file://server/dev/funnychars?:=/assembly.dll", @"//server/dev/funnychars?:=/assembly.dll")]
+        //[TestCase(@"http://server/path/to/assembly.dll", "//server/path/to/assembly.dll")]
+        public void GetAssemblyPathFromCodeBase(string uri, string expectedPath)
+        {
+            string localPath = AssemblyHelper.GetAssemblyPathFromCodeBase(uri);
+            Assert.That(localPath, Is.SamePath(expectedPath));
+        }
+    }
+}
---
+++ src/tclite/Internal/AssemblyHelper.cs
@@ -0,0 +1,92 @@
+﻿// ***********************************************************************
+// Copyright (c) Charlie Poole and TestCentric contributors.
+// Licensed under the MIT License. See LICENSE.txt in root directory.
+// ***********************************************************************
+
+using System;
+using System.Reflection;
+
+namespace TCLite.Framework.Internal
+{
+    /// <summary>
+    /// AssemblyHelper provides static methods for working 
+    /// with assemblies.
+    /// </summary>
+    public class AssemblyHelper
+    {
+
+        /// <summary>
+        /// Gets the path from which the assembly defining a type was loaded.
+        /// </summary>
+        /// <param name="type">The Type.</param>
+        /// <returns>The path.</returns>
+        public static string GetAssemblyPath(Type type)
+        {
+            return GetAssemblyPath(type.Assembly);
+        }
+
+        /// <summary>
+        /// Gets the path from which an assembly was loaded.
+        /// </summary>
+        /// <param name="assembly">The assembly.</param>
+        /// <returns>The path.</returns>
+        public static string GetAssemblyPath(Assembly assembly)
+        {
+            string codeBase = assembly.CodeBase;
+
+            if (IsFileUri(codeBase))
+                return GetAssemblyPathFromCodeBase(codeBase);
+
+            return assembly.Location;
+        }
+
+        /// <summary>
+        /// Gets the path to the directory from which an assembly was loaded.
+        /// </summary>
+        /// <param name="assembly">The assembly.</param>
+        /// <returns>The path.</returns>
+        public static string GetDirectoryName( Assembly assembly )
+        {
+            return System.IO.Path.GetDirectoryName(GetAssemblyPath(assembly));
+        }
+
+        /// <summary>
+        /// Gets the AssemblyName of an assembly.
+        /// </summary>
+        /// <param name="assembly">The assembly</param>
+        /// <returns>An AssemblyName</returns>
+        public static AssemblyName GetAssemblyName(Assembly assembly)
+        {
+            return assembly.GetName();
+        }
+
+        private static bool IsFileUri(string uri)
+        {
+            return uri.ToLower().StartsWith(Uri.UriSchemeFile);
+        }
+
+        // Public for testing purposes
+        public static string GetAssemblyPathFromCodeBase(string codeBase)
+        {
+            // Skip over the file:// part
+            int start = Uri.UriSchemeFile.Length + Uri.SchemeDelimiter.Length;
+
+            bool isWindows = System.IO.Path.DirectorySeparatorChar == '\\';
+
+            if (codeBase[start] == '/') // third slash means a local path
+            {
+                // Handle Windows Drive specifications
+                if (isWindows && codeBase[start + 2] == ':')
+                    ++start;  
+                // else leave the last slash so path is absolute  
+            }
+            else // It's either a Windows Drive spec or a share
+            {
+                if (!isWindows || codeBase[start + 1] != ':')
+                    start -= 2; // Back up to include two slashes
+            }
+
+            return codeBase.Substring(start);
+        }
+    }
+}
