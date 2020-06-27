94da76834bfe9180218e57e0fe3e207646b9b048
Charlie Poole <charliepoole@gmail.com>
2020-06-27 10:31:53 -0700

    Make CollectionConstraint generic

--- src/tclite/Constraints/CollectionConstraint.cs
+++ src/tclite/Constraints/CollectionConstraint.cs
@@ -12,7 +12,7 @@ namespace TCLite.Framework.Constraints
     /// CollectionConstraint is the abstract base class for
     /// constraints that operate on collections.
     /// </summary>
-    public abstract class CollectionConstraint : Constraint
+    public abstract class CollectionConstraint : Constraint<IEnumerable>
     {
         /// <summary>
         /// Construct an empty CollectionConstraint
@@ -44,18 +44,15 @@ namespace TCLite.Framework.Constraints
             return true;
         }
 
-        // /// <summary>
-        // /// Test whether the constraint is satisfied by a given value
-        // /// </summary>
-        // /// <param name="actual">The value to be tested</param>
-        // /// <returns>True for success, false for failure</returns>
-        // public override ConstraintResult ApplyTo<TActual>(TActual actual)
-        // {
-        //     Guard.ArgumentNotNullOfType<IEnumerable>(actual, nameof(actual));
-        //     var enumerable = actual as IEnumerable;
-
-        //     return new ConstraintResult(this, actual, doMatch(enumerable));
-        // }
+        /// <summary>
+        /// Test whether the constraint is satisfied by a given value
+        /// </summary>
+        /// <param name="actual">The value to be tested</param>
+        /// <returns>True for success, false for failure</returns>
+        public override ConstraintResult ApplyTo(IEnumerable actual)
+        {
+            return new ConstraintResult(this, actual, doMatch(actual));
+        }
 
         /// <summary>
         /// Test whether the constraint is satisfied by a given value
